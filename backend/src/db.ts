f2b2a6a711dc09f97015debf787f3c20d0448ee8
kosuke215135 <e215135@ie.u-ryukyu.ac.jp>
2025-05-19 12:09:26 +0900

    feat: mysqlサーバとの通信接続を行うコードの作成

---
+++ backend/src/db.ts
@@ -0,0 +1,16 @@
+import mysql from "mysql2/promise";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = mysql.createPool({
+  host: process.env.FLOW_TETYOU_DB_HOST,
+  user: process.env.FLOW_TETYOU_DB_USER,
+  password: process.env.FLOW_TETYOU_DB_PASSWOR,
+  database: process.env.FLOW_TETYOU_DB_DATABASE_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+export default pool;
