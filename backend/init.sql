a05033a3b1544f4f8cdf5707efbab4a61883e641
kosuke215135 <e215135@ie.u-ryukyu.ac.jp>
2025-05-20 00:09:04 +0900

    chore: 開発で使用するダミーのuser_idを用意した

--- backend/init.sql
+++ backend/init.sql
@@ -1,4 +1,5 @@
-CREATE TABLE notes (
+CREATE DATABASE flow_tetyou
+CREATE TABLE flow_tetyou.notes (
     id INT NOT NULL AUTO_INCREMENT,
     user_id VARCHAR(255) NOT NULL,
     created_at DATE NOT NULL,
