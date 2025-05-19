571df9dcf913d66c323c39d6f16878947bcf85e9
kosuke215135 <e215135@ie.u-ryukyu.ac.jp>
2025-05-20 00:21:26 +0900

    fix: 自動で作成時間が入るように修正

--- backend/init.sql
+++ backend/init.sql
@@ -2,7 +2,7 @@ CREATE DATABASE flow_tetyou
 CREATE TABLE flow_tetyou.notes (
     id INT NOT NULL AUTO_INCREMENT,
     user_id VARCHAR(255) NOT NULL,
-    created_at DATE NOT NULL,
+    created_at DATE DEFAULT (CURRENT_DATE),
     text LONGTEXT NOT NULL,
     PRIMARY KEY (id)
 );
