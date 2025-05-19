4de843c99e49262fad127dc8dff69dced20c3a08
kosuke215135 <e215135@ie.u-ryukyu.ac.jp>
2025-05-20 00:22:05 +0900

    fix: インサート出来るように修正

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import pool from './db';
 import { Request, Response } from "express";
+import dotenv from 'dotenv';
+import path from 'path';
+
+const envPath = path.resolve(__dirname, '../.env.development');
+
+dotenv.config({ path: envPath });
 
 const app = express();
 app.use(express.json()); // JSONをパースする
 
 const PORT = 4000;
+const DUMMY_USER_ID_STRING = process.env.DUMMY_USER_ID_DEV;
 
 // 投稿をDBに記録する
 app.post("/api/note", async (req: Request, res: Response): Promise<void>  => {
@@ -17,8 +24,8 @@ app.post("/api/note", async (req: Request, res: Response): Promise<void>  => {
   }
 
   try {
-    const sql = "INSERT INTO notes (text) VALUES (?)";
-    const [result] = await pool.query(sql, [text]);
+    const sql = "INSERT INTO notes (user_id, text) VALUES (?, ?)";
+    const [result] = await pool.query(sql, [DUMMY_USER_ID_STRING, text]);
     res.status(201).json({ message: "追加成功"});
   } catch (err) {
     console.error("DBエラー", err);
